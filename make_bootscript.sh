35290452f5d42418a9045a78b6455cd4c192a97b
Mark Nijhof <mark.nijhof@cre8ivethought.com>
2015-02-21 22:47:57 +0100

    made it work without the apps level

--- make_bootscript.sh
+++ make_bootscript.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env escript
 %% -*- mode:erlang;tab-width:4;erlang-indent-level:4;indent-tabs-mode:nil -*-
 
-main([]) ->
-    main(dirs_in("apps/*", no_path));
-
-main(Apps) ->
+main([App]) ->
     code:add_paths(dirs_in("deps/*/ebin")),
-    code:add_paths(dirs_in("apps/*/ebin")),
+    code:add_paths(dirs_in("ebin")),
 
-    lists:foreach(fun(App) -> make_boot(App) end, Apps),
+    make_boot(App),
 
     ok.
 
@@ -16,12 +13,12 @@ make_boot(AppName) ->
 
     io:format("~s...~n", [AppName]),
 
-    {ok, [{application, _Name, AppConfig}]} = file:consult(filename:join(["apps", AppName, "src", AppName ++ ".app.src"])),
+    {ok, [{application, _Name, AppConfig}]} = file:consult(filename:join(["src", AppName ++ ".app.src"])),
 
     {applications, Applications} = lists:keyfind(applications, 1, AppConfig),
 
     ReltoolConfig = [{config, {sys, [
-                                     {lib_dirs, ["apps", "deps"]},
+                                     {lib_dirs, ["../", "deps"]},
                                      {erts, [{mod_cond, derived}, {app_file, strip}]},
                                      {rel, AppName, "1", Applications ++ [list_to_atom(AppName)]},
                                      {boot_rel, AppName},
@@ -38,14 +35,10 @@ make_boot(AppName) ->
 
     {ok, Script} = reltool:get_script(Server, AppName),
 
-    file:write_file(filename:join(["apps", AppName, "ebin", AppName ++ ".script"]), io_lib:format("~p.", [Script])),
+    file:write_file(filename:join(["ebin", AppName ++ ".script"]), io_lib:format("~p.", [Script])),
 
-    systools:script2boot(filename:join(["apps", AppName, "ebin", AppName])).
+    systools:script2boot(filename:join(["ebin", AppName])).
 
 dirs_in(Wildcard) ->
     [Dir || Dir <- filelib:wildcard(Wildcard),
     filelib:is_dir(Dir), filelib:is_file(filename:join([Dir, "relx.config"]))].
-
-
-dirs_in(Wildcard, no_path) ->
-    [filename:basename(Dir) || Dir <- dirs_in(Wildcard)].
